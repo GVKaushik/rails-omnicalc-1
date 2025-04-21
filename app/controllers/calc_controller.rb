102db9307da379e800258c1c404f54d6adf258ba
GVKaushik <gvkaushik11@gmail.com>
2025-04-21 08:21:09 +0000

    final

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -19,4 +19,40 @@ class CalcController < ApplicationController
   def random
     render({:template=>"calc_templates/random"})
   end
+
+  def square_results
+    @user_number = params["number"].to_f
+    @result = @user_number*@user_number
+    render({:template=>"calc_templates/square_results"})
+  end
+
+  def square_root_results
+    @user_number = params["user_number"].to_f  # Convert the input to a Float
+    @result = Math.sqrt(@user_number)
+    render({:template=>"calc_templates/square_root_results"})
+  end
+
+  def payment_results
+    @apr = params["user_apr"].to_f
+    @formatted_rate = @apr.to_fs(:percentage, { :precision => 4 })
+    @years = params["user_years"].to_f
+    @principal = params["user_pv"].to_f
+    @rate = @apr/1200
+    @n=-@years*12
+    @numerator = @rate*@principal
+    @denominator = 1 - ((1+@rate)**(@n))
+    @payment = @numerator/@denominator
+    render({:template=>"calc_templates/payment_results"})
+  end
+
+  def random_results
+    @max=params.fetch("user_max").to_f
+    @min=params.fetch("user_min").to_f
+    @rand = rand(@min..@max)
+
+    render({:template=>"calc_templates/random_results"})
+  end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get("/square_root/new", {:controller=>"calc",:action=>"square_root"})
   get("/payment/new", {:controller=>"calc",:action=>"payment"})
   get("/random/new", {:controller=>"calc",:action=>"random"})
+
+  get("/square/results", {:controller=>"calc",:action=>"square_results"})
+  get("/square_root/results", {:controller=>"calc",:action=>"square_root_results"})
+  get("/payment/results", {:controller=>"calc",:action=>"payment_results"})
+  get("/random/results", {:controller=>"calc",:action=>"random_results"})
 end
