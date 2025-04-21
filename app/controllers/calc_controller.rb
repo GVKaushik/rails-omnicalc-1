d9c50f3ea880ed56b2514052466a0a30578d804c
GVKaushik <gvkaushik11@gmail.com>
2025-04-21 08:02:26 +0000

    interim - logic and math left

---
+++ app/controllers/calc_controller.rb
@@ -0,0 +1,22 @@
+class CalcController < ApplicationController
+
+  def homepage
+    render({:template=>"calc_templates/homepage"})
+  end
+
+  def square
+    render({:template=>"calc_templates/square"})
+  end
+
+  def square_root
+    render({:template=>"calc_templates/square_root"})
+  end
+
+  def payment
+    render({:template=>"calc_templates/payment"})
+  end
+
+  def random
+    render({:template=>"calc_templates/random"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  get("/", {:controller=>"calc",:action=>"homepage"})
+  get("/square/new", {:controller=>"calc",:action=>"square"})
+  get("/square_root/new", {:controller=>"calc",:action=>"square_root"})
+  get("/payment/new", {:controller=>"calc",:action=>"payment"})
+  get("/random/new", {:controller=>"calc",:action=>"random"})
 end
